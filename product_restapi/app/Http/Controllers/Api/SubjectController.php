d72af0311279363ed1f113822ae41c74218b3731
Adeleke Hammed <lekhad19@gmail.com>
2021-07-13 14:28:00 +0100

    RestAPI Section CRUD  Part 2

---
+++ product_restapi/app/Http/Controllers/Api/SubjectController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Model\Subject;
+use Illuminate\Http\Request;
+
+class SubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $subject = Subject::all();
+        return response()->json([$subject]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validateData= $request->validate([
+            'class_id' => 'required',
+            'subject_name' => 'required|unique:subjects|max:25',
+            'subject_code' => 'required|unique:subjects|max:25',
+        ]);
+
+        $subject = Subject::create($request->all());
+        return response()->json(['message'=> 'Subject Inserted']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $subject = Subject::findorfail($id);
+        return response()->json(['message' => $subject]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $subject = Subject::findorfail($id);
+        $subject->update($request->all());
+        return response()->json(['message'=> 'Updated Successfully']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Subject::where('id', $id)->delete();
+        return response()->json(['message' => 'Subject Deleted Successfully']);
+    }
+}
